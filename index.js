e55cf1af3a822c48ed742fe24bcf186fe09f3fe8
Svitlana-G <svitlana.golubieva@gmail.com>
2021-07-21 14:24:42 +0200

    gin-page from the API

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
+const fetch = require('node-fetch');
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/gin', (req, res) => {
+    fetch('https://thecocktaildb.com/api/json/v1/1/filter.php?i=gin')
+        .then(res => res.json())
+        .then(json => {
+            console.log(json.drinks)
+            res.render('gin', { data: json.drinks })
+        })
+})
 
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
